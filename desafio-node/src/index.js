de355186e03aa1d1622bb05cd1736a10bd3911ec
Willian Menezes <willianmenezeswms@gmail.com>
2023-07-26 03:06:41 -0300

    integracao com db

--- desafio-node/src/index.js
+++ desafio-node/src/index.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config =  {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodeapp'
+}
+
+
 
 app.get('/', (req, res) => {
-    res.send('Bem vindo ao mundo encantado.....')
+
+    let response = '<h1>Full Cycle Rocks!</h1>';
+
+    const mysql = require('mysql')
+    const connection = mysql.createConnection(config)
+
+    const sql = `INSERT INTO pessoa(nome) VALUES('Willian Menezes')`
+    connection.query(sql)
+
+    connection.query('SELECT nome FROM pessoa', function (error, results, fields) {
+
+        if (error) throw error;
+
+        results.forEach(el => {
+            response += '</br>' + el.nome;
+        });
+
+        connection.end()
+        res.send(response)
+    });
 });
 
 app.listen(port, () => {
