6a66aab9b391716e45fc13a8e6ececab1b3c540e
Willian Menezes <willianmenezeswms@gmail.com>
2023-07-26 19:43:58 -0300

    ajusta chamadas

--- desafio-node/src/index.js
+++ desafio-node/src/index.js
@@ -8,30 +8,22 @@ const config =  {
     database: 'nodeapp'
 }
 
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+connection.query('create table pessoa(id int not null auto_increment, nome varchar(255) not null, primary key(id))')
+
 app.get('/', (req, res) => {
-
     let response = '<h1>Full Cycle Rocks!</h1>';
+    connection.query(`insert into pessoa(nome) values('Willian Menezes')`, function (error, results, fields) {
 
-    const mysql = require('mysql')
-    const connection = mysql.createConnection(config)
+        connection.query('select nome from pessoa', function (error, results, fields) {
 
-    connection.query('select nome from pessoa', function (error, results, fields) {
-
-        if (error) {
-            connection.query('create table pessoa(id int not null auto_increment, nome varchar(255) not null, primary key(id))')
-        }
-
-        connection.query(`insert into pessoa(nome) values('Willian Menezes')`, function (error, results, fields) {
-    
-            connection.query('select nome from pessoa', function (error, results, fields) {
-    
-                results.forEach(el => {
-                    response += '</br>' + el.nome;
-                });
-        
-                connection.end()
-                res.send(response)
+            results.forEach(el => {
+                response += '</br>' + el.nome;
             });
+    
+            connection.end()
+            res.send(response)
         });
     });
 });
