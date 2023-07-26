6977b18d6cb1288d7411d6151383955e4dfe95a1
Willian Menezes <willianmenezeswms@gmail.com>
2023-07-26 20:02:13 -0300

    criado tabela pelo docker compose

---
+++ desafio-node/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS nodeapp;
+USE nodeapp;
+CREATE TABLE IF NOT EXISTS pessoa(id int not null auto_increment, nome varchar(255) not null);
--- desafio-node/src/index.js
+++ desafio-node/src/index.js
@@ -8,24 +8,12 @@ const config =  {
     database: 'nodeapp'
 }
 
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
-connection.query('create table pessoa(id int not null auto_increment, nome varchar(255) not null, primary key(id))')
-
 app.get('/', (req, res) => {
     let response = '<h1>Full Cycle Rocks!</h1>';
-    connection.query(`insert into pessoa(nome) values('Willian Menezes')`, function (error, results, fields) {
-
-        connection.query('select nome from pessoa', function (error, results, fields) {
-
-            results.forEach(el => {
-                response += '</br>' + el.nome;
-            });
-    
-            connection.end()
-            res.send(response)
-        });
-    });
+    const mysql = require('mysql')
+    const connection = mysql.createConnection(config)
+    connection.query(`insert into pessoa(nome) values('Willian Menezes')`)
+    connection.query('select nome from pessoa')
 });
 
 app.listen(port, () => {
