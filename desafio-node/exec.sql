b691dacda9fb6132dbb5a481088d90f6180db22f
Willian Menezes <willianmenezeswms@gmail.com>
2023-07-26 23:12:44 -0300

    teste

--- desafio-node/exec.sql
+++ desafio-node/exec.sql
@@ -1,2 +1,3 @@
+create database if not exists nodeapp;
 use nodeapp;
-create table if not exists pessoa(id int not null auto_increment, nome varchar(255) not null);
+create table if not exists pessoa(id int not null auto_increment, nome varchar(255) not null, primary key(id));
