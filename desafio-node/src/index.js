ec0ba8a1d19a133cdb76c6a4ec28668b6440ff7a
Willian Menezes <willianmenezeswms@gmail.com>
2023-07-26 23:35:37 -0300

    ajusta sql script

--- desafio-node/src/index.js
+++ desafio-node/src/index.js
@@ -12,15 +12,18 @@ app.get('/', (req, res) => {
     let response = '<h1>Full Cycle Rocks!</h1>';
     const mysql = require('mysql')
     const connection = mysql.createConnection(config)
-    connection.query(`insert into pessoa(nome) values('Willian Menezes')`)
-    connection.query('select nome from pessoa', function (error, results, fields) {
 
-        results.forEach(el => {
-            response += '</br>' + el.nome;
-        });
+    connection.query(`insert into pessoa(nome) values('Willian Menezes');`, function (error, results, fields) {
 
-        connection.end()
-        res.send(response)
+        connection.query('select nome from pessoa;', function (error, results, fields) {
+
+            results.forEach(el => {
+                response += '</br>' + el.nome;
+            });
+    
+            connection.end()
+            res.send(response)
+        })
     })
 });
 
